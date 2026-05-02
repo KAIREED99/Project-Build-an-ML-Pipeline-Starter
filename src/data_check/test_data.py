91be954a74cf64e4895126624bfa7fe3afa1c077
kaireed99 <caishengfang99@gmail.com>
2026-05-02 13:05:23 -0500

    filtered out-of-bound coordinates during data cleaning

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -53,7 +53,7 @@ def test_proper_boundaries(data: pd.DataFrame):
     Test proper longitude and latitude boundaries for properties in and around NYC
     """
  
-    idx = data['longitude'].between(-74.25, -73.50) & data['latitude'].between(40.49, 41.2)
+    idx = data['longitude'].between(-74.25, -73.50) & data['latitude'].between(40.50, 41.2)
 
     assert np.sum(~idx) == 0
 
